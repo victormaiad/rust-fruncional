e3a8527c79c657ac8cda25a5aee41bf0c175e5b8
Luis <baskerbyte@gmail.com>
2024-05-12 11:30:04 -0300

    feat: add sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut tabuleiro = [[' '; 3]; 3];
+    let jogador_x = true;
+
+    let mut input = String::new();
+    let stdin = std::io::stdin();
+    match stdin.read_line(&mut input) {
+        Ok(_) => {
+            println!("Vocẽ digitou {input}")
+        }
+        Err(_) => return
+    }
 }
